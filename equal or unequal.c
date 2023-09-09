62a56ff367de3360ef6706e5c9fcebaceb27c080
Navadeep reddy <124556137+Navadeepreddyreddy@users.noreply.github.com>
2023-09-09 13:06:23 +0530

    Add files via upload

---
+++ equal or unequal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n1,n2;
+	printf("enter the number:\n");
+	scanf("%d",&n1);
+	printf("enter the number:\n");
+	scanf("%d",&n2);
+	
+	if(n1 == n2)
+	printf("it is a equal number:\n");
+	else
+	printf("it is not a equal number:\n");
+	return 0;
+}
