bfd1b79d27af160eba77383800e7a0d54253506c
Navadeep reddy <124556137+Navadeepreddyreddy@users.noreply.github.com>
2023-09-09 08:38:28 +0530

    Add files via upload

---
+++ area of circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float pie=3.14,area;
+	int r;
+	printf("enter the radius:");
+	scanf("%d",&r);
+	area =(pie*r*r);
+	printf("area of circle is %f:",area);
+	return 0;
+}
+	
