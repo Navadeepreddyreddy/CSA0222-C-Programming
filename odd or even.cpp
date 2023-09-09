0d0558e2a43d1033075f03f149c4e896aad9e1b4
Navadeep reddy <124556137+Navadeepreddyreddy@users.noreply.github.com>
2023-09-09 09:08:42 +0530

    Add files via upload

---
+++ odd or even.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the number:\n");
+	scanf("%d",&n);
+	if(n%2==0)
+	printf("number is even number:\n");
+	else
+	printf("number is odd number:\n");
+	return 0;
+}
